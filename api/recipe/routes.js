30f5476ba944cd0c820d125beaa6b5568fe0289c
Jennthedragon <jenn.zhang.1318@gmail.com>
2021-01-16 19:54:00 -0500

    fixed routes

--- api/recipe/routes.js
+++ api/recipe/routes.js
@@ -6,6 +6,16 @@ const { getRecipeInfo, getSteps } = require('./utils');
 router.get('/:recipeId', async (req, res) => {
     try {
         const { id } = req.query;
+        const results = await getRecipeInfo(id);
+        res.status(200).send(results);
+    } catch (err) {
+        res.status(400).send(err);
+    }
+});
+
+router.get('/getSteps', async (req, res) => {
+    try {
+        const { id } = req.query;
         const results = await getSteps(id);
         res.status(200).send(results);
     } catch (err) {
